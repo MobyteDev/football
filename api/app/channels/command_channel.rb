e4717daad8348f9bc14a4d595b86cb0b76a05016
Igor <reid09@mail.ru>
2020-08-22 23:43:00 +0300

    fix commandChannel

--- api/app/channels/command_channel.rb
+++ api/app/channels/command_channel.rb
@@ -2,11 +2,19 @@ class CommandChannel < ApplicationCable::Channel
   MESSAGE_TYPES = [1, 2].freeze
 
   def subscribed
+    if !current_user.superuser?
+      current_user.update(online: true)
+      ActionCable.server.broadcast room_id, message: 'Пользователь вошел в флешмоб!', user_id: current_user.id, sender_type: 'System', count_online: User.where(online: true).count
+    end
     stream_from room_id
   end
 
   def unsubscribed
-  
+    if !current_user.superuser?
+      current_user.update(online: false)
+      ActionCable.server.broadcast room_id, message: 'Пользователь вошел из флешмоба!', user_id: current_user.id, sender_type: 'System', count_online: User.where(online: true).count
+    end
+
   end
 
   def receive(data)
@@ -15,7 +23,7 @@ class CommandChannel < ApplicationCable::Channel
     command = data['command']
     return if command.blank? && content.blank?
 
-    ActionCable.server.broadcast room_id, message: content, command: command, sender_type: new_message.sender_type, created_at: Time.zone.now
+    ActionCable.server.broadcast room_id, message: content, command: command, sender_type: "Superuser", time: Time.zone.now, count_online: User.where(online: true).count
   end
 
   protected
