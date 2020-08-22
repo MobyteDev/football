2859157c5da31a10207275d34f8835a713e08ae4
Igor <reid09@mail.ru>
2020-08-22 10:14:03 +0300

    fix

---
+++ api/db/migrate/20200821213304_rename_column_products copy.rb
@@ -0,0 +1,11 @@
+class RenameColumnProducts < ActiveRecord::Migration[5.1]
+  def change
+    add_column :products, :name, :string
+    add_column :products, :subname, :string
+    add_column :products, :price, :string, null: false
+    add_column :products, :category_id, :integer
+    add_index :products, :category_id
+    remove_column :products, :title
+    remove_column :products, :content
+  end
+end
